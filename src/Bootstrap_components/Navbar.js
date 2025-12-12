b4c7fe4365630e92072ff980ad919a2198144fa5
OlehKuts <oleh.kuts@yahoo.com>
2025-12-12 11:05:06 +0200

    delete atttribute activeKey from link in navbar

--- src/Bootstrap_components/Navbar.js
+++ src/Bootstrap_components/Navbar.js
@@ -3,7 +3,7 @@ import Nav from 'react-bootstrap/Nav';
 export const Navbar = ({changePage, pageList}) => {
     return (
         <>
-    <Nav className="justify-content-end" activeKey="/home" style={{margin: "10px auto 30px auto"}}>
+    <Nav className="justify-content-end" style={{margin: "10px auto 30px auto"}}>
         <Nav.Item>
           <Nav.Link href="/home"  onClick={() => changePage(0)}
           style={{color: pageList[0].isVisible ? "deepskyblue" : "gray"}}>Random hero</Nav.Link>
